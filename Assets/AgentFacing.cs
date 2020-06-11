62b158e3f1b5ccea964681fb6937d289d285a1ca
Donovan Westwater <donovan.westwater@gmail.com>
2020-06-11 19:41:48 -0400

    Got rotation to work! The arrow will go in the direction of agent motion. The agents move erractily though, so the arrows may not always give incredibly useufl infomation. But they will tell you where the agent is moving towards. I am going to work on overhauling the back end, and then cleaning up the github

--- Assets/AgentFacing.cs
+++ Assets/AgentFacing.cs
@@ -7,33 +7,40 @@ public class AgentFacing : MonoBehaviour
     //Should probably integrate this code into the Agent Player
     public GameObject aimRet;
     Vector3 oldPos;
-    Vector2 facing;
+    Vector3 facing;
 
     // Start is called before the first frame update
     void Start()
     {
         oldPos = this.transform.position;
-        facing = new Vector2(0, 1);
+        facing = aimRet.transform.position - transform.position;
+        facing.y = 0;//transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 newDir = new Vector3(oldPos.x - transform.position.x, oldPos.y - transform.position.y);
+        Vector3 newDir = new Vector3(oldPos.x - transform.position.x,0, oldPos.z - transform.position.z);//transform.position.y for y param
+        newDir = -newDir;
         float angleTemp = Mathf.Acos(Vector3.Dot(newDir, facing) / (newDir.magnitude * facing.magnitude)); //Is in Radians!
-        oldPos = transform.position;
-       
-        if (Vector3.Cross(facing.normalized, newDir.normalized).z < 0 && angleTemp > Mathf.Abs(0.01f))
+        if (oldPos != transform.position)
         {
-            aimRet.transform.RotateAround(transform.position, new Vector3(0, 1, 0), angleTemp * Mathf.Rad2Deg);
-            facing = newDir;
+            oldPos = transform.position;
         }
-        else if (Vector3.Cross(facing.normalized, newDir.normalized).z > 0 && angleTemp > Mathf.Abs(0.01f))
+        Vector3 product = Vector3.Cross(newDir.normalized, facing.normalized);
+        if (Vector3.Cross(newDir.normalized, facing.normalized).y <= 0 && angleTemp > Mathf.Abs(0.01f))
         {
-            aimRet.transform.RotateAround(transform.position, new Vector3(0, 1, 0), -angleTemp * Mathf.Rad2Deg);
-            facing = newDir;
+            aimRet.transform.RotateAround(transform.position, new Vector3(0, 1, 0), angleTemp*Mathf.Rad2Deg); //* Mathf.Rad2Deg*Time.deltaTime);
+            facing = aimRet.transform.position - transform.position;
+            facing.y = 0; //transform.position.y;
         }
-        if (Input.GetKey(KeyCode.D)) transform.position += new Vector3(10*Time.deltaTime,0,0); 
+        else if (Vector3.Cross(newDir.normalized, facing.normalized).y > 0 && angleTemp > Mathf.Abs(0.01f))
+        {
+            aimRet.transform.RotateAround(transform.position, new Vector3(0, 1, 0), -angleTemp * Mathf.Rad2Deg); //* Mathf.Rad2Deg * Time.deltaTime);
+            facing = aimRet.transform.position - transform.position;
+            facing.y = 0; //transform.position.y;
+        }
+        //if (Input.GetKey(KeyCode.D)) transform.position += new Vector3(10*Time.deltaTime,0,1*Time.deltaTime); 
         
     }
 }
