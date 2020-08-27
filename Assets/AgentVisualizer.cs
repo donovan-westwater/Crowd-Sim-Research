c5d3c5d7978c82255729fbb60ad82e156846f31e
Donovan Westwater <donovan.westwater@gmail.com>
2020-08-27 16:38:05 -0400

    I adjusted the cylinders to have the arrows on top of them, and cleaned up the AgentVisualizer file to not have any unused code

--- Assets/AgentVisualizer.cs
+++ Assets/AgentVisualizer.cs
@@ -6,45 +6,41 @@ public class AgentVisualizer : MonoBehaviour
 {
     // Start is called before the first frame update
    
-    Material circle; //Unused
+    
     public GameObject cylinder;
+    public GameObject facing;
     public int id;
-    static Vector4[] array = new Vector4[300]; //Unused
+   
     public static bool visualMode = false; //Are the agents cylinders or models
     void Start()
     {
         //Gives the agent's cylinders a color based on the id
         if (id % 5 == 0) cylinder.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
-        else if(id % 2 == 0) cylinder.GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
+        else if(id % 2 == 0) cylinder.GetComponent<Renderer>().material.SetColor("_Color", Color.cyan);
         else if(id % 3 == 0) cylinder.GetComponent<Renderer>().material.SetColor("_Color", Color.yellow);
     }
 
     // Update is called once per frame
     void Update()
     {
-        array[id] = this.transform.position;
-        
+
+
         //Turns on cylinders of agents when visualMode is true
         if (visualMode)
         {
-            
+            facing.SetActive(true);
             cylinder.SetActive(true);
-        }else cylinder.SetActive(false); //Turns cylinders off when false
-       
+        }
+        else
+        {
+            facing.SetActive(false);
+            cylinder.SetActive(false); //Turns cylinders off when false
+        }
     }
 
     public void setVisualMode(bool b)
     {
         visualMode = b;
-        if (!b)
-        {
-            //Unused (Delete this section)
-            Vector4[] clear = new Vector4[300];
-            for (int i = 0; i < 300; i++)
-            {
-                clear[i] = new Vector4(9999, 9999, 9999, 9999);
-            }
-            //circle.SetVectorArray("_aLocs", clear);
-        }
+      
     }
 }
